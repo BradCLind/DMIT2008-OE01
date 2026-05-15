404720d8fb5a52e74ca035a18ab6e5934b094827
oaao <oliver+github@abolish.golf>
2026-05-14 18:45:40 -0600

    rev [01 - js review]: ex01 - render out data to HTML cards

--- 01 - javascript review/examples/01 - expense tracker - basic review/instructor/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/instructor/js/main.js
@@ -3,10 +3,35 @@ import expenses from './expense-data.js';
 
 // 2. grab relevant DOM elements
 const expenseContainer = document.getElementById('expense-container')
+const searchTerm = document.getElementById('searchbox')
 
-// 3. [skeleton]: render out data into a grid of cards
+// 3. render out data into a grid of cards
 function renderExpenses(expenseData) {
-    
+  // first, clear out existing HTML for the container (because we're about to re-render it)
+  expenseContainer.innerHTML = "";
+
+  // then, take our array of data, and render out a card for each one
+  // for a given expense, add a new card containing that data to the expenseContainer's inner HTML
+  expenseData.forEach(
+    (expense) => {
+      expenseContainer.innerHTML += `
+      <div class="card" id="${expense.id}">
+        <div class="header">
+          <div>
+            <div class="title">${expense.title}</div>
+            <div class="meta category">${expense.category}</div>
+          </div>
+          <div class="amount">${expense.amount}</div>
+        </div>
+        <div class="meta date">${expense.date}</div>
+        <div class="actions">
+          <button class="edit-btn" id="${expense.id}">Edit</button>
+          <button class="delete-btn" id="${expense.id}">Delete</button>
+        </div>
+      </div>
+    `
+    }
+  );
 }
 
 // 4. call the function to actually do the render
